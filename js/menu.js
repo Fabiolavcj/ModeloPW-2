320da96ef6e2e2c4b8ed22d4dd75c46be6fdda51
Fabiola Castro <fabiolavcj2102@gmail.com>
2020-09-23 12:12:52 -0500

    Se creo el primer repositorio del Segundo Modelo de una Pagina Web

---
+++ js/menu.js
@@ -0,0 +1,11 @@
+var ompwmenu = document.getElementById("ompw-menu");
+var menu = document.getElementById("menu");
+
+ompwmenu.addEventListener("click", function(){
+    menu.classList.toggle("mostrar");
+})
+/*Codigo de JavaS para revision porque no me funciona*/
+   /* document.getElementById("submenuu").onclick = function() {mysubmenuu()};
+function mysubmenuu() {
+    document.getElementById("mySubmenuu").classList.toggle("show");
+}*/
